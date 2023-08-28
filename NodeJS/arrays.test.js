e2ecf8417a5564a8ca07ff70504cb93f7cd63092
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-08-28 23:59:21 +0300

    Fix naming

--- NodeJS/arrays.test.js
+++ NodeJS/arrays.test.js
@@ -5,10 +5,10 @@ const assert = require('node:assert');
 
 const arrays = require('./arrays.js');
 
-test('Example 3', (text) => {
+test('Example 3', (test) => {
   assert.strictEqual(typeof arrays.sample([1, 2, 3]), 'number');
 });
 
-test('Example 4', async (text) => {
+test('Example 4', async (test) => {
   assert.strictEqual(arrays.shuffle([1, 2, 3]).length, 3);
 });
--- NodeJS/utils.test.js
+++ NodeJS/utils.test.js
@@ -5,12 +5,12 @@ const assert = require('node:assert');
 
 const utils = require('./utils.js');
 
-test('Example 1', (text) => {
+test('Example 1', (test) => {
   assert.strictEqual(utils.add(4, 1), 5);
   assert.strictEqual(utils.sub(5, 2), 3);
 });
 
-test('Example 2', async (text) => {
+test('Example 2', async (test) => {
   assert.strictEqual(utils.add(4, 1), 5);
   assert.strictEqual(utils.sub(5, 2), 3);
 });
